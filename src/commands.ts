9803fd1558aa6556db0be8b706587d839d42ccdb
Ioakeim Mingo <canonshai@gmail.com>
2025-07-15 16:31:49 +0100

    Implement inspect and pokedex commands

--- src/commands.ts
+++ src/commands.ts
@@ -5,6 +5,8 @@ import type { CLICommand } from "./state.js";
 import { commandMapNext, commandMapPrev } from "./command_map.js";
 import { commandExplore } from "./command_explore.js";
 import { commandCatch } from "./command_catch.js";
+import { commandInspect } from "./command_inspect.js";
+import { commandPokedex } from "./command_pokedex.js";
 
 export function getCommands(): Record<string, CLICommand> {
   return {
@@ -37,6 +39,16 @@ export function getCommands(): Record<string, CLICommand> {
       name: "catch <pokemon_name>",
       description: "Attempt to catch a pokemon",
       callback: commandCatch,
+    },
+    inspect: {
+      name: "inspect <pokemon_name>",
+      description: "View details about a caught pokemon",
+      callback: commandInspect,
+    },
+    pokedex: {
+      name: "pokedex",
+      description: "See all the pokemon you've caught",
+      callback: commandPokedex,
     },
   };
 }
