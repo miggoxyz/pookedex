259b57592245b9b0cff1c1ad26d92bae68db9266
Ioakeim Mingo <canonshai@gmail.com>
2025-07-15 16:29:31 +0100

    Implement catch command

--- src/commands.ts
+++ src/commands.ts
@@ -4,6 +4,7 @@ import { commandExit } from "./command_exit.js";
 import type { CLICommand } from "./state.js";
 import { commandMapNext, commandMapPrev } from "./command_map.js";
 import { commandExplore } from "./command_explore.js";
+import { commandCatch } from "./command_catch.js";
 
 export function getCommands(): Record<string, CLICommand> {
   return {
@@ -31,6 +32,11 @@ export function getCommands(): Record<string, CLICommand> {
       name: "explore",
       description: "Explore location area by name",
       callback: commandExplore,
+    },
+    catch: {
+      name: "catch <pokemon_name>",
+      description: "Attempt to catch a pokemon",
+      callback: commandCatch,
     },
   };
 }
